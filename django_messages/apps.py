e63f80a35367109a21c6e4f0578cd58801afc435
solve-coagulate <ben.burns@gmail.com>
2025-06-10 04:28:21 +0800

    Specify default_auto_field

--- django_messages/apps.py
+++ django_messages/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 class DjangoMessagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'django_messages'
     verbose_name = _('Messages')
